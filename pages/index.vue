45ced58d51cb613abdf533e230438ac29b6abce3
davy bolivar <davy.bolivar@wavecell.com>
2020-01-21 01:05:06 +0800

    feat: initialize nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    HELLO WORLD
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+</style>
