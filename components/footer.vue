1d96d48d56a1f73972be9ec210fc220b75aed9de
davy bolivar <davy.bolivar@wavecell.com>
2020-01-22 22:56:11 +0800

    feat: setup layout

---
+++ components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    FOOTER
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  @apply tw-border-t tw-border-gray-500 tw-py-2 tw-w-full tw-mx-auto;
+  position: relative;
+  max-width: 1230px;
+}
+</style>
