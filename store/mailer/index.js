5dcf6e5dd7fd06be63abf182c2310696b2c3e3da
davy bolivar <davy.bolivar@wavecell.com>
2020-02-08 23:08:28 +0800

    fix: update store structure

--- store/mailer/index.js
+++ store/mailer/index.js
@@ -1,26 +1,22 @@
-import actions from './actions'
+import mailerActions from './actions'
 
-export default {
-  namespaced: true,
+export const state = () => ({
+  loading: false,
+  status: 0 // 0:none; 1:sent; -1:failed
+})
 
-  state: {
-    loading: false,
-    status: 0 // 0:none; 1:sent; -1:failed
-  },
-
-  getters: {
-    getLoading: (state) => state.loading,
-    getStatus: (state) => state.status
-  },
-
-  mutations: {
-    SET_LOADING: (st, bool) => {
-      st.loading = bool
-    },
-    SET_STATUS: (st, status) => {
-      st.status = status
-    }
-  },
-
-  actions
+export const getters = {
+  getLoading: (state) => state.loading,
+  getStatus: (state) => state.status
 }
+
+export const mutations = {
+  SET_LOADING: (st, bool) => {
+    st.loading = bool
+  },
+  SET_STATUS: (st, status) => {
+    st.status = status
+  }
+}
+
+export const actions = mailerActions
